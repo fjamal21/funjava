28c18c39969b050feb5ea9b50a8f034c9eb2040d
fjamal21 <jamalferhan@gmail.com>
2016-04-03 10:19:25 -0700

    Update BinarySearchTree.java

--- Algorithms/src/main/java/com/algorithm/datastructures/BinarySearchTree.java
+++ Algorithms/src/main/java/com/algorithm/datastructures/BinarySearchTree.java
@@ -1,232 +1,358 @@
 package com.algorithm.datastructures;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Comparator;
+import java.util.Iterator;
 
-/**
- * An implementation of binary trees
- * @author morin
- *
- * @param <Node>
- */
-public class BinaryTree<Node extends BinaryTree.BTNode<Node>> {
+public class BinarySearchTree<Node extends BinarySearchTree.BSTNode<Node,T>, T> extends
+		BinaryTree<Node> implements SSet<T> {
+
+	protected Comparator<T> c;
 	
-	public static class BTNode<Node extends BTNode<Node>> {
-		public Node left;
-		public Node right;
-		public Node parent;	
+	public static class BSTNode<Node extends BSTNode<Node,T>,T>
+		extends BinaryTree.BTNode<Node> {
+		T x;
 	}
 
 	/**
-	 * Used to make a mini-factory
+	 * The number of nodes (elements) currently in the tree
 	 */
-	protected Node sampleNode;
+	protected int n;
 	
-	/**
-	 * The root of this tree
-	 */
-	protected Node r;
-
-	/**
-	 * This tree's "null" node
-	 */
-	protected Node nil;
-
-	/**
-	 * Create a new instance of this class
-	 * @param sampleNode - a sample of a node that can be used
-	 * to create a new node in newNode()
-	 * @param nil - a node that will be used in place of null
-	 */
-	public BinaryTree(Node sampleNode, Node nil) {
-		this.sampleNode = sampleNode;
-		this.nil = nil;
-		r = nil;
+	protected Node newNode(T x) {
+		Node u = super.newNode();
+		u.x = x;
+		return u;
 	}
 
-	/**
-	 * Create a new instance of this class
-	 * @param sampleNode - a sample of a node that can be used
-	 * to create a new node in newNode()
-	 */
-	public BinaryTree(Node sampleNode) {
-		this.sampleNode = sampleNode;
+	public BinarySearchTree(Node sampleNode, Node nil, Comparator<T> c) {
+		super(sampleNode, nil);
+		this.c = c; 
 	}
-	
-	/**
-	 * Allocate a new node for use in this tree
-	 * @return
-	 */
-	@SuppressWarnings({"unchecked"})
-	protected Node newNode() {
-		try {
-			Node u = (Node)sampleNode.getClass().newInstance();
-			u.parent = u.left = u.right = nil;
-			return u;
-		} catch (Exception e) {
-			return null;
-		}
+
+	public BinarySearchTree(Node sampleNode, Comparator<T> c) {
+		super(sampleNode);
+		this.c = c; 
 	}
-	
-	/**
-	 * Compute the depth (distance to the root) of u
-	 * @param u
-	 * @return the distanct between u and the root, r
-	 */
-	public int depth(Node u) {
-		int d = 0;
-		while (u != r) {
-			u = u.parent;
-			d++;
-		}
-		return d;
+
+	public BinarySearchTree(Node sampleNode) {
+		this(sampleNode, new DefaultComparator<T>());
 	}
-	
+
 	/**
-	 * Compute the size (number of nodes) of this tree
-	 * @warning uses recursion so could cause a stack overflow
-	 * @return the number of nodes in this tree
+	 * Search for a value in the tree
+	 * @return the last node on the search path for x
 	 */
-	public int size() {
-		return size(r);
-	}
-	
-	/**
-	 * @return the size of the subtree rooted at u
-	 */
-	protected int size(Node u) {
-		if (u == nil) return 0;
-		return 1 + size(u.left) + size(u.right);
-	}
-	
-	/**
-	 * Compute the number of nodes in this tree without recursion
-	 * @return
-	 */
-	public int size2() {
-		Node u = r, prev = nil, next;
-		int n = 0;
-		while (u != nil) {
-			if (prev == u.parent) {
-				n++;
-				if (u.left != nil) next = u.left;
-				else if (u.right != nil) next = u.right;
-				else next = u.parent;
-			} else if (prev == u.left) {
-				if (u.right != nil) next = u.right;
-				else next = u.parent;
+	protected Node findLast(T x) {
+		Node w = r, prev = nil;
+		while (w != nil) {
+			prev = w;
+			int comp = c.compare(x, w.x);
+			if (comp < 0) {
+				w = w.left;
+			} else if (comp > 0) {
+				w = w.right;
 			} else {
-				next = u.parent;
+				return w;
 			}
-			prev = u;
-			u = next;
 		}
+		return prev;
+	}
+	
+	/**
+	 * Search for a value in the tree
+	 * @return the last node on the search path for x
+	 */
+	protected Node findGENode(T x) {
+		Node w = r, z = nil;
+		while (w != nil) {
+			int comp = c.compare(x, w.x);
+			if (comp < 0) {
+				z = w;
+				w = w.left;
+			} else if (comp > 0) {
+				w = w.right;
+			} else {
+				return w;
+			}
+		}
+		return z;
+	}
+
+	protected T findEQ(T x) {
+		Node u = r;
+		while (u != nil) {
+			int comp = c.compare(x, u.x);
+			if (comp < 0) 
+				u = u.left;
+			else if (comp > 0)
+				u = u.right;
+			else
+				return u.x;
+		}
+		return null;
+	}
+
+	protected T find(T x) {
+		Node w = r, z = nil;
+		while (w != nil) {
+			int comp = c.compare(x, w.x);
+			if (comp < 0) {
+				z = w;
+				w = w.left;
+			} else if (comp > 0) {
+				w = w.right;
+			} else {
+				return w.x;
+			}
+		}
+		return z == nil ? null : z.x;
+	}
+
+	public T findGE(T u) {
+		if (u == null) { // find the minimum value
+			Node w = r;
+			if (w == nil) return null;
+			while (w.left != nil)
+				w = w.left;
+			return w.x;
+		}
+		Node w = findGENode(u);
+		return w == nil ? null : w.x;
+	}
+
+	/**
+	 * Search for a value in the tree
+	 * @return the last node on the search path for x
+	 */
+	protected Node findLTNode(T x) {
+		Node u = r, z = nil;
+		while (u != nil) {
+			int comp = c.compare(x, u.x);
+			if (comp < 0) {
+				u = u.left;
+			} else if (comp > 0) {
+				z = u;
+				u = u.right;
+			} else {
+				return u;
+			}
+		}
+		return z;
+	}
+
+	public T findLT(T x) {
+		if (x == null) { // find the maximum value
+			Node w = r;
+			if (w == nil) return null;
+			while (w.right != nil)
+				w = w.right;
+			return w.x;
+		}
+		Node w = findLTNode(x);
+		return w == nil ? null : w.x;
+	}
+
+	/**
+	 * Add the node u as a child of node p -- ASSUMES p has no child
+	 * where u should be added
+	 * @param p
+	 * @param u
+	 * @return true if the child was added, false otherwise
+	 */
+	protected boolean addChild(Node p, Node u) {
+		if (p == nil) {
+			r = u;              // inserting into empty tree
+		} else {
+			int comp = c.compare(u.x, p.x);
+			if (comp < 0) {
+				p.left = u;
+			} else if (comp > 0) {
+				p.right = u;
+			} else {
+				return false;   // u.x is already in the tree
+			}
+			u.parent = p;
+		}
+		n++;
+		return true;		
+	}
+
+	/**
+	 * Add a new value
+	 * @param x
+	 * @return
+	 */
+	public boolean add(T x) {
+		Node p = findLast(x);
+		return addChild(p, newNode(x));		
+	}
+
+	/**
+	 * Add a new value
+	 * @param x
+	 * @return
+	 */
+	public boolean add(Node u) {
+		Node p = findLast(u.x);
+		return addChild(p, u);		
+	}
+
+	/**
+	 * Remove the node u --- ASSUMING u has at most one child
+	 * @param u
+	 */
+	protected void splice(Node u) {
+		Node s, p;
+		if (u.left != nil) {
+			s = u.left;
+		} else {
+			s = u.right;
+		}
+		if (u == r) {
+			r = s;
+			p = nil;
+		} else {
+			p = u.parent;
+			if (p.left == u) {
+				p.left = s;
+			} else {
+				p.right = s; 
+			}
+		}
+		if (s != nil) {
+			s.parent = p;
+		}
+		n--;
+	}
+	
+	/**
+	 * Remove the node u from the binary search tree
+	 * @param u
+	 */
+	protected void remove(Node u) {
+		if (u.left == nil || u.right == nil) {
+			splice(u);
+		} else {
+			Node w = u.right;
+			while (w.left != nil) 
+				w = w.left;
+			u.x = w.x;
+			splice(w);
+		}
+	}
+	
+	/**
+	 * Do a left rotation at u
+	 * @param u
+	 */
+	protected void rotateLeft(Node u) {
+		Node w = u.right;
+		w.parent = u.parent;
+		if (w.parent != nil) {
+			if (w.parent.left == u) {
+				w.parent.left = w;
+			} else {
+				w.parent.right = w;
+			}
+		}
+		u.right = w.left;
+		if (u.right != nil) {
+			u.right.parent = u;
+		}
+		u.parent = w;
+		w.left = u;
+		if (u == r) { r = w; r.parent = nil; }
+	}	
+	
+	/**
+	 * Do a right rotation at u
+	 * @param u
+	 */
+	protected void rotateRight(Node u) {
+		Node w = u.left;
+		w.parent = u.parent;
+		if (w.parent != nil) {
+			if (w.parent.left == u) {
+				w.parent.left = w;
+			} else {
+				w.parent.right = w;
+			}
+		}
+		u.left = w.right;
+		if (u.left != nil) {
+			u.left.parent = u;
+		}
+		u.parent = w;
+		w.right = u;
+		if (u == r) { r = w; r.parent = nil; }
+	}
+
+	/**
+	 * Remove a node
+	 * @param x
+	 * @return
+	 */
+	public boolean remove(T x) {
+		Node u = findLast(x);
+		if (u != nil && c.compare(x,u.x) == 0) {
+			remove(u);
+			return true;
+		}
+		return false;
+	}
+	
+	public String toString() {
+		String s = "[";
+		Iterator<T> it = iterator();
+		while (it.hasNext()) {
+			s += it.next().toString() + (it.hasNext() ? "," : "");
+		}
+		s += "]";
+		return s;
+	}
+	
+	public Iterator<T> iterator(Node u) {
+		class BTI implements Iterator<T> {
+			protected Node w, prev;
+			public BTI(Node iw) {
+				w = iw;
+			}
+			public boolean hasNext() {
+				return w != nil;
+			}
+			public T next() {
+				T x = w.x;
+				prev = w;
+				w = nextNode(w);
+				return x;
+			}
+			public void remove() {
+				// FIXME: This is a bug.  remove() methods have to be changed
+				
+				BinarySearchTree.this.remove(prev);
+			}
+		}
+		return new BTI(u);
+	}
+
+	public Iterator<T> iterator() {
+		return iterator(firstNode());
+	}
+
+	public Iterator<T> iterator(T x) {
+		return iterator(findGENode(x));
+	}
+	
+	public int size() {
 		return n;
 	}
 
-	/**
-	 * Compute the maximum depth of any node in this tree
-	 * @return the maximum depth of any node in this tree
-	 */
-	public int height() {
-		return height(r);
-	}
-	
-	/**
-	 * @return the size of the subtree rooted at u
-	 */
-	protected int height(Node u) {
-		if (u == nil) return -1;
-		return 1 + Math.max(height(u.left), height(u.right));
-	}
-
-	
-	/**
-	 * @return
-	 */
-	public boolean isEmpty() {
-		return r == nil;
-	}
-	
-	/**
-	 * Make this tree into the empty tree
-	 */
 	public void clear() {
-		r = nil;
-	}
-	
-	/**
-	 * Demonstration of a recursive traversal
-	 * @param u
-	 */
-	public void traverse(Node u) {
-		if (u == nil) return;
-		traverse(u.left);
-		traverse(u.right);
+		super.clear();
+		n = 0;
 	}
 
-	/**
-	 * Demonstration of a non-recursive traversal
-	 */
-	public void traverse2() {
-		Node u = r, prev = nil, next;
-		while (u != nil) {
-			if (prev == u.parent) {
-				if (u.left != nil) next = u.left;
-				else if (u.right != nil) next = u.right;
-				else next = u.parent;
-			} else if (prev == u.left) {
-				if (u.right != nil) next = u.right;
-				else next = u.parent;
-			} else {
-				next = u.parent;
-			}
-			prev = u;
-			u = next;
-		}
+	public Comparator<? super T> comparator() {
+		return c;
 	}
-
-	/**
-	 * Demonstration of a breadth-first traversal
-	 */
-	public void bfTraverse() {
-		Queue<Node> q = new LinkedList<Node>();
-		if (r != nil) q.add(r);
-		while (!q.isEmpty()) {
-			Node u = q.remove();
-			if (u.left != nil) q.add(u.left);
-			if (u.right != nil) q.add(u.right);
-		}
-	}
-
-	/**
-	 * Find the first node in an in-order traversal
-	 * @return the first node reported in an in-order traversal
-	 */
-	public Node firstNode() {
-		Node w = r;
-		if (w == nil) return nil;
-		while (w.left != nil)
-			w = w.left;
-		return w;
-	}
-	
-	/**
-	 * Find the node that follows w in an in-order traversal
-	 * @param w
-	 * @return the node that follows w in an in-order traversal
-	 */
-	public Node nextNode(Node w) {
-		if (w.right != nil) {
-			w = w.right;
-			while (w.left != nil)
-				w = w.left;
-		} else {
-			while (w.parent != nil && w.parent.left != w)
-				w = w.parent;
-			w = w.parent;
-		}
-		return w;
-	}
-
 }
